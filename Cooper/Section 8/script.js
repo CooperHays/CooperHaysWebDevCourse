d90fb10be3fc5443edee0532010aa017d70a0fdd
Michael J Cooper <ballermjc@gmail.com>
2017-04-25 15:15:21 -0600

    Unit 8 Completed

---
+++ Cooper/Section 8/script.js
@@ -0,0 +1,14 @@
+// alert("Hello from the JS File!");
+
+// var userName = prompt("What is your name?");
+//
+// alert("Nice to meet you, " + userName);
+// console.log("Also great to meet you, " + userName);
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
+alert("You have been alive for approximately " + age*365.25 + " days.")
