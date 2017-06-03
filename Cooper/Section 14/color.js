369a8d4372332932bb186ee6bf473ec6a062640a
Michael J Cooper <ballermjc@gmail.com>
2017-06-03 10:46:59 -0600

    through section 14

---
+++ Cooper/Section 14/color.js
@@ -0,0 +1,11 @@
+var button = document.querySelector("button");
+// var isPurple = false;
+button.addEventListener("click", function(){
+  document.body.classList.toggle("purple");
+  // if(isPurple) {
+  //   document.body.style.background = "white";
+  // } else {
+  //   document.body.style.background = "purple";
+  // }
+
+});
